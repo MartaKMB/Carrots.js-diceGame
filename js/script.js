dfab63a6b134a9f8cbacce141574a027aa5ec62d
MartaKMB <muszkamarta@interia.pl>
2017-10-25 12:53:16 +0200

    Work on start page. Button to confirm name and draw first player

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
+const btn_first_player = document.querySelector('#btn_first_player');
+const span_first_player = document.querySelector('#first_player_name');
+
 const btn_start = document.querySelector('#btn_start');
 const start_view = document.querySelector('#container_start');
+
+btn_first_player.addEventListener('click', (e) => {
+   let player_name = document.querySelector('#input_name').value;
+    
+    let draw_num = Math.round(Math.random());
+    
+    if(player_name === '') {
+        player_name = 'Tajemniczy Gość';
+    } 
+    
+    if(draw_num === 0) {
+        span_first_player.innerHTML = player_name;
+    } else {
+        span_first_player.innerHTML = 'komputer';
+    }  
+    
+});
 
 btn_start.addEventListener('click', (e) => {
     //alert('test');
