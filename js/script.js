3769454adab74398f8069d253da8006921921027
MartaKMB <muszkamarta@interia.pl>
2017-10-25 11:01:34 +0200

    Start work at dice game with js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btn_start = document.querySelector('#btn_start');
+const start_view = document.querySelector('#container_start');
+
+btn_start.addEventListener('click', (e) => {
+    //alert('test');
+    
+    start_view.setAttribute( 'class', 'invisible' );
+});
