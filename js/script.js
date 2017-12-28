6ffc535389a7b1098802cbdffbb57c0a1b4d85be
MartaKMB <muszkamarta@interia.pl>
2017-12-28 16:43:55 +0100

    Come back to app. Work at option when user throw dices and computer make choice

--- js/script.js
+++ js/script.js
@@ -22,103 +22,186 @@ btn_first_player.addEventListener('click', (e) => {
 });
 
 const span_round = document.querySelector('#round');
-let counter = 1;
+let counter = 0;
+
+const div_sum_a = document.querySelector('#sum_a');
+const div_sum_b = document.querySelector('#sum_b');
+
+const span_player_points = document.querySelector('#player_points');
+const span_computer_points = document.querySelector('#computer_points');
+let computer_points = 0;
+let player_points = 0;
 
 const btn_throw = document.querySelector('#btn_throw');
 
-const div_dice_1 = document.querySelector('#dice_1');
-const div_dice_2 = document.querySelector('#dice_2');
-const div_dice_3 = document.querySelector('#dice_3');
+// first throw
+const div_dice_1_1 = document.querySelector('#dice_1_1');
+const div_dice_2_1 = document.querySelector('#dice_2_1');
+const div_dice_3_1 = document.querySelector('#dice_3_1');
 
-let sum_a = 0;
-let checkBiggerLower = true;
-const btn_bigger = document.querySelector('#btn_bigger');
-const btn_lower = document.querySelector('#btn_lower');
+// message about decision
+const div_comp_msg = document.querySelector('#comp_msg')
+
+//second throw
+const div_dice_1_2 = document.querySelector('#dice_1_2');
+const div_dice_2_2 = document.querySelector('#dice_2_2');
+const div_dice_3_2 = document.querySelector('#dice_3_2');
+
+
+const btn_check = document.querySelector('#btn_check');
+const radio_bigger = document.querySelector('#bigger');
+const radio_lower = document.querySelector('#lower');
+
+
+function playerRound(){
+    
+    if(counter >= 5) return;
+    
+    counter ++;
+    span_round.innerHTML = counter;
+    
+    
+    btn_throw.disabled = false;
+    btn_check.disabled = true;
+    
+    console.log(counter);
+
+    btn_throw.addEventListener('click', randomDiceNumbers(1));
+    
+    const sum_a = sumDiceNumbers(div_dice_1_1.innerHTML, div_dice_2_1.innerHTML, div_dice_3_1.innerHTML);
+    
+    div_sum_a.innerHTML = 'pierwszy rzut suma: ' + sum_a;
+
+    const computerDecision = biggerOrLower();
+    
+    if(computerDecision === true) {
+        div_comp_msg.innerHTML= computerDecision + ' suma oczek z kolejnego rzutu będzie większa';
+    } else {
+        div_comp_msg.innerHTML= computerDecision + ' suma oczek z kolejnego rzutu będzie mniejsza';
+    }
+    
+    
+        
+    const secondThrow = randomDiceNumbers(2);
+    btn_throw.addEventListener('click', randomDiceNumbers(2));
+    
+    const sum_b = sumDiceNumbers(div_dice_1_2.innerHTML, div_dice_2_2.innerHTML, div_dice_3_2.innerHTML);
+    
+    div_sum_b.innerHTML = 'drugi rzut suma: ' + sum_b;
+    
+    // false: next sum will be lower; true: next sum will be bigger
+    
+    if(computerDecision === true && sum_b > sum_a) {
+        console.log(computerDecision + ' komputer miał rację suma oczek z drugiego rzutu jest większa');
+        computer_points++;
+        span_computer_points.innerHTML = computer_points;
+    } else if(computerDecision === true && sum_b < sum_a) {
+        console.log(computerDecision + ' komputer NIE miał racji suma oczek z drugiego rzutu jest mniejsza');
+        player_points++;
+        span_player_points.innerHTML = player_points;
+    } else if(computerDecision === false && sum_b < sum_a) {
+        console.log(computerDecision + ' komputer miał rację suma oczek z drugiego rzutu jest mniejsza');
+        computer_points++;
+        span_computer_points.innerHTML = computer_points;
+    } else if(computerDecision === false && sum_b > sum_a) {
+        console.log(computerDecision + ' komputer NIE miał racji suma oczek z drugiego rzutu jest większa');
+        player_points++;
+        span_player_points.innerHTML = player_points;
+    } else {
+        console.log('może sumy są takie same, albo coś poszło nie tak');
+    }
+
+    
+}
 
 
 btn_start.addEventListener('click', (e) => {
 
     // start_view.setAttribute( 'class', 'invisible' );
-    span_round.innerHTML = counter;
+//    span_round.innerHTML = counter;
 
-    if (span_first_player.innerHTML === 'komputer') {
-
-        randomDiceNumbers();
-
-        //      btn_throw.disabled = true;
-    } else {
-        //      btn_throw.disabled = false;
-    }
-
-});
-
-btn_throw.addEventListener('click', (e) => {
-
-    span_round.innerHTML = counter;
-    counter++;
-
-    const dice_value_1 = randomDiceNumber();
-    const dice_value_2 = randomDiceNumber();
-    const dice_value_3 = randomDiceNumber();
-
-    div_dice_1.innerHTML = dice_value_1;
-    div_dice_2.innerHTML = dice_value_2;
-    div_dice_3.innerHTML = dice_value_3;
-
-    const sum_b = sumDiceNumbers(dice_value_1, dice_value_2, dice_value_3);
-
-    if (sum_a !== 0) {
-        if (sum_a < sum_b && checkBiggerLower === true) {
-            document.querySelector('#test').innerHTML = sum_b + ' ta jest większa od poprzedniej ' + sum_a + ' i tak kliknąłeś';
-        } else if (sum_a < sum_b && checkBiggerLower === false) {
-            document.querySelector('#test').innerHTML = sum_b + ' ta jest większa od poprzedniej ' + sum_a + ' i tak NIE kliknąłeś - kliknąłeś, że będzie mniejsza';
-        } else if (sum_a > sum_b && checkBiggerLower === false) {
-            document.querySelector('#test').innerHTML = sum_a + ' poprzednia jest większa ' + sum_b + ' i tak kliknąłeś';
-        } else if (sum_a > sum_b && checkBiggerLower === true) {
-            document.querySelector('#test').innerHTML = sum_a + ' poprzednia jest większa ' + sum_b + ' i tak NIE kliknąłeś - kliknąłeś, że będzie większa';
-        }
-
-    } else {
-        document.querySelector('#test').innerHTML = 'nie ma sumy do której można porównać';
-    }
+//    if (span_first_player.innerHTML === 'komputer') {
+//
+//        randomDiceNumbers();
+//        sum_b = sumDiceNumbers(div_dice_1.innerHTML, div_dice_2.innerHTML, div_dice_3.innerHTML);
+//        btn_throw.disabled = true; 
+//        btn_check.disabled = false;
+//    } else {
+//        
+//            playerRound();
+        
+        
+//    }
+    span_round.innerHTML = 1;
+    span_player_points.innerHTML = player_points;
+    span_computer_points.innerHTML = computer_points;
 
 });
 
-btn_bigger.addEventListener('click', (e) => {
+function throwFunction() {
+//    if(counter > 10) return;
+//    
+//    span_round.innerHTML = Math.floor(counter/2);
+//    counter ++;
+    
+//    randomDiceNumbers();
+    
+//    const sum_a = sumDiceNumbers(div_dice_1.innerHTML, div_dice_2.innerHTML, div_dice_3.innerHTML);
+//    
+//    return sum_a;
+}
 
+btn_throw.addEventListener('click', playerRound);
+
+
+function clickCheck() {
+    if(bigger.checked === true) {
+        document.querySelector('#test').innerHTML = 'większy';
+    } else if (lower.checked === true) {
+        document.querySelector('#test').innerHTML = 'mniejszy';
+    } else {
+        document.querySelector('#test').innerHTML = 'dziwadła dziwadełka';
+    }
+    
     const dice_value_1 = div_dice_1.innerHTML;
     const dice_value_2 = div_dice_2.innerHTML;
     const dice_value_3 = div_dice_3.innerHTML;
 
     sum_a = sumDiceNumbers(dice_value_1, dice_value_2, dice_value_3);
+}
 
-    checkBiggerLower = true;
+btn_check.addEventListener('click', clickCheck);
 
-});
-
-btn_lower.addEventListener('click', (e) => {
-
-    const dice_value_1 = div_dice_1.innerHTML;
-    const dice_value_2 = div_dice_2.innerHTML;
-    const dice_value_3 = div_dice_3.innerHTML;
-
-    sum_a = sumDiceNumbers(dice_value_1, dice_value_2, dice_value_3);
-
-    checkBiggerLower = false;
-
-});
 
 function randomNumber() {
     let random_nr = Math.floor((Math.random() * 6) + 1);
     return random_nr;
 }
 
-function randomDiceNumbers() {
-    div_dice_1.innerHTML = randomNumber();
-    div_dice_2.innerHTML = randomNumber();
-    div_dice_3.innerHTML = randomNumber();
+function randomDiceNumbers(round) {
+    if(round == 1) {
+        div_dice_1_1.innerHTML = randomNumber();
+        div_dice_2_1.innerHTML = randomNumber();
+        div_dice_3_1.innerHTML = randomNumber();
+    } else if(round == 2) {
+        div_dice_1_2.innerHTML = randomNumber();
+        div_dice_2_2.innerHTML = randomNumber();
+        div_dice_3_2.innerHTML = randomNumber();
+    } else {
+        console.log('coś poszło nie tak - funkcja randomDiceNumbers(round)');
+    }
+    
 }
 
 function sumDiceNumbers(a, b, c) {
     return parseInt(a) + parseInt(b) + parseInt(c);
+}
+
+function biggerOrLower() {
+    let decision = Math.floor((Math.random() * 2) + 1);
+    if(decision === 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
